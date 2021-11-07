e5848c115ac179845ef3bf6e326d8886bee4d021
nensi <NENSI.DOBARIA@GMAIL.COM>
2021-11-07 09:38:15 -0600

    Adding the election audit code.

--- Pypoll.py
+++ Pypoll.py
@@ -5,19 +5,59 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
     # Read and print the header row.
-    headers = next(file_reader)
-    print(headers)
+    # headers = next(file_reader)
+    # print(headers)
 
-    i = 1
+    # i = 1
     for row in file_reader:
-        if i <= 10:
-            print(row)
-            i=i+1
+        total_votes += 1
+        #print(total_votes)
+
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+         candidate_options.append(candidate_name)
+         candidate_votes[candidate_name] = 0
+
+        candidate_votes[candidate_name] += 1
+print(candidate_votes)
     #headers = next(file_reader)
     #print(headers)
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+     votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+     vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+     #print(f"{candidate_name}: received {vote_percentage}% of the vote.")
+#  To do: print out each candidate's name, vote count, and percentage of votes to the terminal.
+
+    # Determine winning vote count and candidate
+    # Determine if the votes is greater than the winning count.
+     if (votes > winning_count) and (vote_percentage > winning_percentage):
+         # If true then set winning_count = votes and winning_percent =
+         # vote_percentage.
+         winning_count = votes
+         winning_percentage = vote_percentage
+         # And, set the winning_candidate equal to the candidate's name.
+         winning_candidate = candidate_name
+   #print out the winning candidate, vote count and percentage to  terminal   
+      
+     print(f"{candidate_name} : {vote_percentage:.1f}% ({votes:,})\n")
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)   
--- Python_practice.py
+++ Python_practice.py
@@ -1,46 +1,66 @@
-print ("Hello World")
-type(73.81)
-type("Hello World")
-my_list = [ ]
-counties = ["Arapahoe","Denver","Jefferson"]
-counties.insert(1, "El Paso")
-counties.remove("Arapahoe")
-print(counties)
+# print ("Hello World")
+# type(73.81)
+# type("Hello World")
+# my_list = [ ]
+# counties = ["Arapahoe","Denver","Jefferson"]
+# counties.insert(1, "El Paso")
+# counties.remove("Arapahoe")
+# print(counties)
 
 
 
-counties = ["Arapahoe","Denver","Jefferson"]
-if counties[1] == 'Denver':
-    print(counties[1])
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if counties[1] == 'Denver':
+#     print(counties[1])
 
 
-for county in counties:
-    print(county)
-
-    numbers = [0, 1, 2, 3, 4]
-for num in numbers:
-    print(num)
-
-
-counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
-# for county in counties_dict:
-#     print(county) 
-#     for county in counties_dict.keys():
-#      print(county)
-# for county in counties_dict.keys():
+# for county in counties:
 #     print(county)
-#     for voters in counties_dict.values():
-#      print(voters)
-#print(counties_dict["Arapahoe"])
 
-for countykey in counties_dict:
-  print(counties_dict[countykey])
-for key, value in counties_dict.items():
-    print(key,"County has ", value, "regestered voters.")
+#     numbers = [0, 1, 2, 3, 4]
+# for num in numbers:
+#     print(num)
 
-import datetime 
-now = datetime.datetime.now()
-print("The time right now is", now)
-import csv
-dir(csv)
-dir({'Arapahoe': 422829, 'Denver': 463353, 'Jefferson': 432438})
+
+# counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+# # for county in counties_dict:
+# #     print(county) 
+# #     for county in counties_dict.keys():
+# #      print(county)
+# # for county in counties_dict.keys():
+# #     print(county)
+# #     for voters in counties_dict.values():
+# #      print(voters)
+# #print(counties_dict["Arapahoe"])
+
+# for countykey in counties_dict:
+#   print(counties_dict[countykey])
+# for key, value in counties_dict.items():
+#     print(key,"County has ", value, "regestered voters.")
+
+# import datetime 
+# now = datetime.datetime.now()
+# print("The time right now is", now)
+# import csv
+# dir(csv)
+# dir({'Arapahoe': 422829, 'Denver': 463353, 'Jefferson': 432438})
+
+# Incorporate the random library
+import random
+
+# Print Title
+print("Let's Play Rock Paper Scissors!")
+
+# Specify the three options
+options = ["r", "p", "s"]
+
+# Computer Selection
+computer_choice = random.choice(options)
+
+# User Selection
+user_choice = input("Make your Choice: (r)ock, (p)aper, (s)cissors? ")
+
+ # Run Conditionals
+
+
+
